d33701853076ae8e00aa5236267dc288b89d96a4
DavidCT <davidcuervotunon@gmail.com>
2021-06-11 10:44:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ document.onclick(function () {
     alert('Esto es una prueba para github');
 });
 
+/*Esta es una función con la que vamos a obtener los clientes de la base de datos
+    Este es el comentario largo de la funión*/
 function ObtenerClientes() {
-    const prueba = document.querySelector('UnaPruebaDeSelector');
+    const prueba = document.querySelector('UnaPruebaDeSelector'); //Esto es un comentario básico
     var cadena = 'Esto es una prueba de cadena que va para git hub';
 
 };
